bf3952c1cfcd10d0946df9688e2b0e13ee0dc396
7x <info@se7enx.com>
2026-03-26 19:39:28 -0700

    Stop swapping legacy kernel handlers in console

--- bundle/EventListener/LegacyKernelListener.php
+++ bundle/EventListener/LegacyKernelListener.php
@@ -9,13 +9,9 @@ namespace eZ\Bundle\EzPublishLegacyBundle\EventListener;
 use eZ\Publish\Core\MVC\Legacy\LegacyEvents;
 use eZ\Publish\Core\MVC\Legacy\Event\PreResetLegacyKernelEvent;
 use eZINI;
-use Symfony\Component\Console\ConsoleEvents;
-use Symfony\Component\Console\Event\ConsoleCommandEvent;
 use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use eZ\Publish\Core\MVC\Symfony\MVCEvents;
-use eZ\Publish\Core\MVC\Symfony\Event\ScopeChangeEvent;
 
 /**
  * Resets eZINI when the Legacy Kernel is reset.
@@ -39,7 +35,6 @@ class LegacyKernelListener implements EventSubscriberInterface
     {
         return [
             LegacyEvents::PRE_RESET_LEGACY_KERNEL => 'onKernelReset',
-            ConsoleEvents::COMMAND => 'onConsoleCommand',
         ];
     }
 
@@ -54,23 +49,4 @@ class LegacyKernelListener implements EventSubscriberInterface
         );
     }
 
-    public function onConfigScopeChange(ScopeChangeEvent $event)
-    {
-        $this->resetKernelHandler();
-    }
-
-    public function onConsoleCommand(ConsoleCommandEvent $event)
-    {
-        $this->resetKernelHandler();
-
-        $this->eventDispatcher->addListener(MVCEvents::CONFIG_SCOPE_CHANGE, [$this, 'onConfigScopeChange'], -1);
-        $this->eventDispatcher->addListener(MVCEvents::CONFIG_SCOPE_RESTORE, [$this, 'onConfigScopeChange'], -1);
-    }
-
-    private function resetKernelHandler()
-    {
-        $legacyHandlerCLI = $this->container->get('ezpublish_legacy.kernel_handler.cli');
-        $this->container->set('ezpublish_legacy.kernel_handler', $legacyHandlerCLI);
-        $this->container->set('ezpublish_legacy.kernel_handler.web', $legacyHandlerCLI);
-    }
 }
