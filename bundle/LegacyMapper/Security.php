b2957a22d7659af2b31a26ca4cead76cad41bc8e
7x <info@se7enx.com>
2026-03-26 23:20:23 -0700

    Fix getCurrentUser() -> getPermissionResolver()->getCurrentUserReference()->getUserId() for eZ Platform 3 API

--- bundle/LegacyMapper/Security.php
+++ bundle/LegacyMapper/Security.php
@@ -88,10 +88,10 @@ class Security implements EventSubscriberInterface
             return;
         }
 
-        $currentUser = $this->repository->getCurrentUser();
+        $userId = $this->repository->getPermissionResolver()->getCurrentUserReference()->getUserId();
         $event->getLegacyKernel()->runCallback(
-            static function () use ($currentUser) {
-                $legacyUser = eZUser::fetch($currentUser->id);
+            static function () use ($userId) {
+                $legacyUser = eZUser::fetch($userId);
                 eZUser::setCurrentlyLoggedInUser($legacyUser, $legacyUser->attribute('contentobject_id'), eZUser::NO_SESSION_REGENERATE);
             },
             false,
